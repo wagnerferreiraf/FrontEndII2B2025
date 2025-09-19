f11315044cf849cd455a6e7fb05667f6fe812719
wagnerferreiraf <professorwagnerferreira@gmail.com>
2025-09-19 16:05:39 -0400

    Aula do dia 19/09/2025

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,28 @@
+'use client';
+import { useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  // let nr = 0;
+  const [nr, setNr] = useState(0);
+
+  const somar = () => {
+    setNr(nr + 1);
+    console.log(nr);
+  }
+
+  const subtrair = () => {
+    setNr(nr - 1);
+    console.log(nr);
+  }
+
   return (
     <div>
       <main>
         <h1 className={styles.titulo}>Hello World!</h1>
+        <button onClick={() => { somar() }} className={styles.btn}>+</button>
+        <button onClick={() => { subtrair() }} className={styles.btn}>-</button>
+        <p>Número: {nr}</p>
       </main>
     </div>
   );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,42 @@
+'use client';
+import { useState } from 'react';
 import styles from './Header.module.css';
 import Link from 'next/link';
 
 export default function Header() {
+    const [show, setShow] = useState(false);
     return (
-        <header className={styles.header}>
-            <nav>
-                <ul className={styles.ulMenu}>
-                    <li className={styles.liMenu}><Link href='/'>Home</Link ></li>
-                    <li className={styles.liMenu}><Link href='/produtos'>Produtos</Link >
-                        <ul className={styles.ulSubMenu}>
-                            <li className={styles.liSubMenu}><Link href='/produtos'>Cadastrar</Link ></li>
-                            <li className={styles.liSubMenu}><Link href='/produtos'>Relatório</Link > </li>
+        <>
+            <header className={styles.headerMobile}>
+                <button onClick={() => { setShow(!show) }}>X</button>
+                {show &&
+                    <nav className={styles.nav}>
+                        <ul className={styles.ulMenu}>
+                            <li className={styles.liMenu}><Link href='/'>Home</Link ></li>
+                            <li className={styles.liMenu}><Link href='/produtos'>Produtos</Link >
+                                <ul className={styles.ulSubMenu}>
+                                    <li className={styles.liSubMenu}><Link href='/produtos'>Cadastrar</Link ></li>
+                                    <li className={styles.liSubMenu}><Link href='/produtos'>Relatório</Link > </li>
+                                </ul>
+                            </li>
                         </ul>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+                    </nav>
+                }
+            </header>
+
+            <header className={styles.header}>
+                <nav>
+                    <ul className={styles.ulMenu}>
+                        <li className={styles.liMenu}><Link href='/'>Home</Link ></li>
+                        <li className={styles.liMenu}><Link href='/produtos'>Produtos</Link >
+                            <ul className={styles.ulSubMenu}>
+                                <li className={styles.liSubMenu}><Link href='/produtos'>Cadastrar</Link ></li>
+                                <li className={styles.liSubMenu}><Link href='/produtos'>Relatório</Link > </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
     );
 }
