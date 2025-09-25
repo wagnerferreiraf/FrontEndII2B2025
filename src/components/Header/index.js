94f7ebb216440a3233b6446c06bf14917f1f8848
wagnerferreiraf <professorwagnerferreira@gmail.com>
2025-09-25 15:38:30 -0400

    Aula do dia 25/09/2025

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,21 @@
 import { useState } from 'react';
 import styles from './Header.module.css';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Header() {
     const [show, setShow] = useState(false);
+    const [icon, setIcon] = useState('/images/open.png');
+
+    const alterarIcone = () => {
+        if (show) setIcon('/images/open.png');
+        else setIcon('/images/close.png')
+        setShow(!show);
+    }
     return (
         <>
             <header className={styles.headerMobile}>
-                <button onClick={() => { setShow(!show) }}>X</button>
+                <div className={styles.btn} onClick={() => { alterarIcone() }}><Image src={icon} width={30} height={30} alt='Ícone menu'></Image></div>
                 {show &&
                     <nav className={styles.nav}>
                         <ul className={styles.ulMenu}>
